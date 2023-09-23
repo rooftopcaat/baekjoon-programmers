bf15349ab9854f867fec43ef6bca27405d4380e6
roofTopCaat <143480639+rooftopcaat@users.noreply.github.com>
2023-09-23 22:43:07 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        백슬래시 출력의 경우 \+1
+        한줄코드의 경우 \n을 통해 가독성을 올린다
+        */
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
